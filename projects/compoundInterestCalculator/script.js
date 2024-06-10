73a346fa87dfea04c13a8eb9341fac26a6c576ac
pandajakob <jakobm.dev@gmail.com>
2024-06-10 21:25:41 +0200

    UI and yes

--- projects/compoundInterestCalculator/script.js
+++ projects/compoundInterestCalculator/script.js
@@ -1,15 +1,27 @@
+const timeHorizon = document.getElementById("timeHorizon");
+const initialInvestment = document.getElementById("initInvestment");
+let yield = 2;
+const tableElement = document.querySelector('table')
 
-const button = document.getElementById('button')
-
-function newElement(event) {
-    const parent = document.getElementById('ul')
-
-    for (let i = 0; i < 4; i++) {
-        const element = document.createElement('li');
-        let number = (i * 3)/4;
-        element.innerHTML = `${i} ` + `${event.target}`;
-        parent.appendChild(element);
-    }
+const updateTextInput = (value) => {
+    document.getElementById("rangeValue").innerHTML = value + "%"
+    yield = value/100+1;
+    console.log("year" + timeHorizon.value);
+    console.log("initialInvestment" + initialInvestment.value);
 }
-button.addEventListener('click', newElement);
+
+function addToTable() {   
+    tableElement.innerHTML = ""
+    for (let i = 0; i<timeHorizon.value; i++) {
+        let trElement = document.createElement('tr');
+        let yearElement = document.createElement('th');
+        yearElement.innerHTML = i;
+        let amountElement = document.createElement('th');
+        amountElement.innerHTML = (i*initialInvestment.value*yield);
+        trElement.appendChild(yearElement)
+        trElement.appendChild(amountElement)
+        tableElement.appendChild(trElement)
+    }
+
+};
 
