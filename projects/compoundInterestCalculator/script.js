49705d66e2f4482cb4e2ac221a45e6d7b43f7334
pandajakob <jakobm.dev@gmail.com>
2024-06-11 09:16:16 +0200

    dataPoint object

--- projects/compoundInterestCalculator/script.js
+++ projects/compoundInterestCalculator/script.js
@@ -1,27 +1,45 @@
 const timeHorizon = document.getElementById("timeHorizon");
 const initialInvestment = document.getElementById("initInvestment");
-let yield = 2;
+const monthlyContribution = document.getElementById("monthlyContribution")
+let yield = 0.0;
 const tableElement = document.querySelector('table')
 
 const updateTextInput = (value) => {
     document.getElementById("rangeValue").innerHTML = value + "%"
     yield = value/100+1;
-    console.log("year" + timeHorizon.value);
-    console.log("initialInvestment" + initialInvestment.value);
+    console.log("year: " + timeHorizon.value);
+    console.log("initialInvestment: " + initialInvestment.value);
 }
+updateTextInput(0);
 
-function addToTable() {   
-    tableElement.innerHTML = ""
-    for (let i = 0; i<timeHorizon.value; i++) {
+function addToTable() {
+    tableElement.innerHTML = "<tr> <th> Year </th> <th> Amount </th> </tr>"
+    let totalValue = parseFloat(initialInvestment.value);
+    const yearlyContribution = parseFloat(monthlyContribution.value*12);
+
+    for (let year = 1; year <= timeHorizon.value; year++) {
+        if (year === 1) {
+            totalValue = (totalValue + yearlyContribution) * yield;
+        } else {
+            totalValue = totalValue*yield+yearlyContribution;
+        }
+
         let trElement = document.createElement('tr');
+
         let yearElement = document.createElement('th');
-        yearElement.innerHTML = i;
+        yearElement.innerHTML = year;
+
         let amountElement = document.createElement('th');
-        amountElement.innerHTML = (i*initialInvestment.value*yield);
+        amountElement.innerHTML = "$"+Math.round(totalValue);
+
         trElement.appendChild(yearElement)
         trElement.appendChild(amountElement)
         tableElement.appendChild(trElement)
     }
 
 };
-
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        addToTable();
+    }
+});
