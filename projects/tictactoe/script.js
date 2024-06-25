b008687831db428584445fa23e60fe2e53660821
pandajakob <jakobm.dev@gmail.com>
2024-06-25 17:47:45 +0200

    Minimax algorithm implemented

--- projects/tictactoe/script.js
+++ projects/tictactoe/script.js
@@ -31,21 +31,21 @@ function boxClicked(e) {
         if (validateGame) {
             setTimeout(playComputerTurn, 200);
         }
+
     }
     console.log("spaces", spaces)
-
 };
 
 boxes.forEach(box => {
-    box.addEventListener("click",  boxClicked)
+    box.addEventListener("click", boxClicked)
 });
 
 
 const restartGame = () => {
     spaces.fill(null);
-    boxes.forEach(box => { 
+    boxes.forEach(box => {
         box.innerHTML = "";
-        box.style.backgroundColor =  "var(--bgColor)"
+        box.style.backgroundColor = "var(--bgColor)"
     })
     playerScoreElement.innerHTML = playerScore;
     computerScoreElement.innerHTML = computerScore;
@@ -53,36 +53,168 @@ const restartGame = () => {
 }
 
 
-// adds eventListeners for each gameField
-const playComputerTurn = () => {
-    let freeSpaces = getFreeSpaces(spaces);
-    
-    if (freeSpaces.length > 1) {
-        const i = minimax(freeSpaces);
-        if (i !== null) {
-        boxes[i].innerHTML = O_Text;
-        spaces[i] = O_Text;
-        currentPlayer = X_Text;
+const showWinningCombo = (combo) => {
+    combo.forEach(i => {
+        boxes[i].style.backgroundColor = "var(--darkBG)";
+    })
+}
+
+
+
+const getFreeSpaces = (array) => {
+    let freeSpaces = []
+    array.forEach((space, i) => {
+        if (!space) {
+            freeSpaces.push(i)
         }
+    })
+    return freeSpaces;
+}
+
+
+
+const checkGame = (gamespaces) => {
+    let game = true;
+    winningCombos.some(combo => {
+        let [a, b, c] = combo;
+        if (gamespaces[a] && gamespaces[a] === gamespaces[b] && gamespaces[a] === gamespaces[c]) {
+            // let winner = spaces[a] === X_Text ? rating = -1 : rating = 1;
+            game = false;
+            return true
+        } else if (getFreeSpaces(gamespaces).length === 0) {
+            game = false;
+            return true
+        }
+        return false
+    })
+    return game;
+}
+
+const playComputerTurn = () => {
+    if (checkGame(spaces)) {
+        let freeSpaces = getFreeSpaces(spaces);
+
+
+
+        let children = getChildren(spaces, O_Text);
+        console.log("children", getChildren(spaces, currentPlayer))
+        let minimaxArray = []
+
+        children.forEach(child => {
+            console.log("child", child)
+            minimaxArray.push(minimax(child, 6, X_Text));
+        })
+        let i = minimaxArray.indexOf(Math.max(...minimaxArray));
+        console.log("i", i)
+        let bestChoice = children[i];
+        
+        console.log("best choice", bestChoice)
+
+        console.log("minimaxarray", minimaxArray)
+
+        /*
+        if (i) {
+            boxes[i].innerHTML = O_Text;
+            spaces[i] = O_Text;
+            currentPlayer = X_Text;
+        } 
+            */
+
+        bestChoice.forEach((field, index) => {
+            boxes[index].innerHTML = field;
+            spaces[index] = field;
+
+        })
+
+        currentPlayer = X_Text;
+
+
+
     }
     validateGame();
 };
 
+
+
+const getChildren = (gameSpaces, player) => {
+    let freeSpaces = getFreeSpaces(gameSpaces)
+
+    let array = [];
+    for (let i = 0; i < freeSpaces.length; i++) {
+        gameSpaces[freeSpaces[i]] = player;
+        array.push(new Array(...gameSpaces));
+        gameSpaces[freeSpaces[i]] = null;
+    }
+    // console.log("Children", array)
+    return array;
+
+}
+
+
+const evaluate = (gamespaces) => {
+    let rating = 0;
+    winningCombos.forEach(combo => {
+        let [a, b, c] = combo;
+        if (gamespaces[a] && gamespaces[a] === gamespaces[b] && gamespaces[a] === gamespaces[c]) {
+            gamespaces[a] == X_Text ? rating = -Infinity : rating = Infinity;
+        } else if (!spaces.includes(null)) {
+            rating = 0
+        }
+    })
+    return rating;
+}
+
+const minimax = (currentSpaces, depth, player) => {
+    // console.log("depth", depth)
+    let children = getChildren(currentSpaces, player);
+
+    if (depth === 0 || !checkGame(currentSpaces)) {
+        let evaluation = evaluate(currentSpaces);
+        console.log("eval player", evaluation, player)
+        return evaluation;
+    } 
+        if (player === O_Text) {
+            let maxEval = -Infinity;
+            children.forEach(child => {
+                let evaluation = minimax(child, depth - 1, X_Text)
+                maxEval = Math.max(maxEval, evaluation);
+            })
+            return maxEval
+
+        } else if (player === X_Text) {
+            let minEval = Infinity;
+
+            children.forEach(child => {
+                let evaluation = minimax(child, depth - 1, O_Text)
+                minEval = Math.min(minEval, evaluation);
+            })
+            return minEval;
+
+        }
+}
+
+
+
+
+
 const validateGame = () => {
     let gameContinues = true;
+
     winningCombos.some(combo => {
         let [a, b, c] = combo;
         if (spaces[a] && spaces[a] === spaces[b] && spaces[a] === spaces[c]) {
             showWinningCombo(combo);
             spaces[a] === X_Text ? playerScore++ : computerScore++;
-            setTimeout(() => {window.alert(`${spaces[a]} wins!`)
-            restartGame();
+            setTimeout(() => {
+                window.alert(`${spaces[a]} wins!`)
+                restartGame();
             }, 100)
             gameContinues = false;
             return true
         } else if (!spaces.includes(null)) {
-            setTimeout(() => {window.alert(`Tie`)
-            restartGame();
+            setTimeout(() => {
+                window.alert(`Tie`)
+                restartGame();
             }, 100)
             gameContinues = false;
             return true
@@ -93,105 +225,13 @@ const validateGame = () => {
     return gameContinues;
 };
 
-const showWinningCombo = (combo) => {
-    combo.forEach(i => {
-        boxes[i].style.backgroundColor = "var(--darkBG)";
-    }) 
-}
 
-const checkGame = (spaces) => {
-    let rating = 0;
-    winningCombos.forEach(combo => {
-        let [a, b, c] = combo;
 
-        if (spaces[a] && spaces[a] === spaces[b] && spaces[a] === spaces[c]) {
-           let winner = spaces[a] === X_Text ? rating = -1 : rating = 1;
-            console.log("winner",winner)
-        }
-    })
-    return rating
-}
 
-const idOfMax = (array) => {
+
+const indexOfMax = (array) => {
     let max = Math.max(...array);
-    console.log("max", max)
     let maxIndex = array.indexOf(max);
-    console.log("maxIndex", maxIndex)
-
     return maxIndex;
 
-}
-
-const getFreeSpaces = (array) => {
-
-    let freeSpaces = []
-    array.forEach((space, i) => {
-        if (!space) {
-            freeSpaces.push(i)
-        }
-    })
-
-    return freeSpaces;
-
-}
-
-
-const goThrough = (array) => {
-    for (let i = 0; i < array.length; i++) {
-
-    }
-}
-
-const minimax = (freeSpaces) => {
-    let returnID = null;
-    let testSpaces = []
-    spaces.forEach(space => {
-        testSpaces.push(space)
-    }) 
-
-
-    let optionRating = [];
-
-
-    for (let i1 = 0; i1 < freeSpaces.length; i1++) {
-        console.log("")
-        let ratings = []
-        
-        testSpaces[freeSpaces[i1]] = O_Text;
-        
-        console.log("Test space for O:", freeSpaces[i1]);
-       
-       
-        let newFreeSpaces = getFreeSpaces(testSpaces);
-
-        console.log("newfreespaces", newFreeSpaces);
-            if (newFreeSpaces.length > 1) {
-                for (let i2 = 0; i2 < newFreeSpaces.length; i2++) {
-                    console.log("Test space for X:", newFreeSpaces[i2]);
-                    testSpaces[newFreeSpaces[i2]] = X_Text;
-                    ratings.push(checkGame(testSpaces))
-                    testSpaces[newFreeSpaces[i2]] = null;
-                }
-                optionRating.push(Math.min(...ratings));
-            } else {
-                optionRating.push(checkGame(testSpaces))
-            }
-        console.log("ratings: ", ratings);
-      
-        ratings = [];
-        testSpaces[freeSpaces[i1]] = null;
-    }
-
-    console.log("optionRating", optionRating)
-
-    console.log("testspaces", testSpaces)
-
-    let id = idOfMax(optionRating);
-
-    returnID = freeSpaces[id];
-
-    console.log("minimax", returnID)
-
-    return returnID;
-    
 }
